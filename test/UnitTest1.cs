f635e2c75ba91322b539c8d89a9714308d6b4963
Tom Parrish <tom.parrish@dotdigital.com>
2020-01-27 18:52:23 +0000

    Add initial xUnit test project

---
+++ test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ceilingfish.Caching.Distributed.Json.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
